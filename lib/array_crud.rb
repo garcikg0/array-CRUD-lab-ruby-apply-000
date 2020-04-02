9933ba3a03c8e548bc70d33d55f26fec153c8912
Kevin Garcia <kg.gar91@gmail.com>
2020-04-02 22:29:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_cats = ["north", "south", "east"]
-  famous_cats.push("Joe")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
