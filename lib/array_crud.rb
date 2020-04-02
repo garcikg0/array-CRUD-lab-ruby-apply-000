58c4e763a5bf44021a13df6645b995011139bb60
Kevin Garcia <kg.gar91@gmail.com>
2020-04-02 22:19:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array(friends)
-  friends = [Aidan, Tom, Wes, Mike]
+  friends = ["Aidan", "Tom", "Wes", "Mike"]
 end
 
 def add_element_to_end_of_array(array, element)
